6991d5b2463c9f4f7726cf1fc48f94d072ddcce5
Eimantas <eim.kasp@gmail.com>
2013-11-28 13:30:26 +0200

    angular list words filter, order implemented

--- views/home.js
+++ views/home.js
@@ -1,32 +1,43 @@
 apuokasApp.controller('homeCtrl', ['$scope', function ($scope) {
     var json = {
-        "modules":
+        "irasai":
             [
                 {
-                    "zodis":"mama",
-                    "raides":[{"id":0, "title":"m"},
+                    "zodis":{zodis: "bama"},
+                    "raides":[
+                        {"id":0, "title":"b"},
                         {"id":1, "title":"a"},
                         {"id":2, "title":"m"},
                         {"id":3, "title":"a"}]
                 },
 
                 {
-                    "zodis":"tetis",
-                    "raides":[{"id":0, "title":"t"},
+                    "zodis":{"zodis" : "tes"},
+                    "raides":[
+                        {"id":0, "title":"t"},
                         {"id":1,"title":"e"},
                         {"id":2,"title":"t"},
                         {"id":3,"title":"i"},
                         {"id":4,"title":"s"},
 
                     ]
+                },
+
+                {
+                    "zodis":{"zodis": "alytus"},
+                    "raides":[
+                        {"id":0, "title":"a"},
+                        {"id":1,"title":"l"},
+                        {"id":2,"title":"y"},
+                        {"id":3,"title":"t"},
+                        {"id":4,"title":"u"},
+                        {"id":5,"title":"s"},
+
+                    ]
                 }
             ]
     };
 
-    $scope.ocw = json;
-
-//    $scope.main = zodziai;
-//    $scope.orderProp = 'sudetingumas';
+    $scope.zodynas = json;
     $scope.hello = "Hello, world";
-   // console.log($scope.zodziai[0].raide);
 }]);
