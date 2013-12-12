f2ea087edb5e4d78dabc2fcb6e17e7da6ba92eb1
Declawler <r.milasevicius@gmail.com>
2013-12-12 13:31:53 +0200

    mode1 error

--- controllers/mode1.js
+++ controllers/mode1.js
@@ -1,21 +1,39 @@
 apuokasApp.controller('oneCtrl', ['$scope', function ($scope) {
-    var json = {
+    $scope.zodynas = {
         "irasai":
             [
                 {
                     "zodis":{"zodis" : "ąžuolas"},
                     "raides":[
-                        {"id":0,"title":"ą"},
-                        {"id":1,"title":"ž"},
-                        {"id":2,"title":"u"},
-                        {"id":3,"title":"o"},
-                        {"id":4,"title":"l"},
-                        {"id":5,"title":"a"},
-                        {"id":6,"title":"s"},
+                        {"id":0,"pav":"", "empty": "true"},
+                        {"id":1,"pav":"ž", "empty": "false"},
+                        {"id":2,"pav":"u", "empty": "false"},
+                        {"id":3,"pav":"o", "empty": "false"},
+                        {"id":4,"pav":"l", "empty": "false"},
+                        {"id":5,"pav":"a", "empty": "false"},
+                        {"id":6,"pav":"s", "empty": "false"}
+                    ],
+                    "trukstamosRaides":[
+                        {"id":0, "pav":"a", "empty": "false", "correct": false},
+                        {"id":1, "pav":"ą", "empty": "false", "correct": true}
                     ]
-                }
+                }/*,
+                {
+                    "zodis":{"zodis" : "samanos"},
+                    "raides":[
+                        {"id":0,"pav":"s"},
+                        {"id":1,"pav":""},
+                        {"id":2,"pav":"m"},
+                        {"id":3,"pav":"a"},
+                        {"id":4,"pav":"n"},
+                        {"id":5,"pav":"o"},
+                        {"id":6,"pav":"s"}
+                    ],
+                    "trukstamosRaides":[
+                        {"id":0, "pav":"a", "correct": true},
+                        {"id":1, "pav":"ą", "correct": false}
+                    ]
+                }*/
             ]
     };
-
-    $scope.zodynas = json;
 }]);
